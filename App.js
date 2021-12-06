cec34cf56998c68f9a17e818cfb200f966333a29
Nikolai Nanev <niki4etooo@gmail.com>
2021-12-06 16:14:34 +0200

    added simple test question with test answers

--- App.js
+++ App.js
@@ -2,20 +2,53 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends React.Component {
+	
+	constructor(props){
+		super(props);
+		
+		this.styles = StyleSheet.create({
+		  container: {
+			flex: 1,
+			backgroundColor: '#fff',
+			alignItems: 'center',
+			justifyContent: 'center',
+		  },
+		 
+		  answers: {
+			display: 'grid',
+			gridTemplateRows: 'repeat(2, 1fr)',
+			gridTemplateColumns: 'repeat(2, 1fr)',
+			gap: '50px',
+			
+			padding: '30px',
+		  },
+		  
+		  question: {
+			  fontSize: 32,
+			  fontWeight: 'bold',
+		  },
+		  
+		  answer: {
+			  fontSize: 24,
+		  },
+		});
+		
+		this.bytes = 4;
+	}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	render () {
+		return (
+			<View style={this.styles.container}>
+			  <Text style={this.styles.question}>How much bits are in {this.bytes} bytes?</Text>
+			  <View style={this.styles.answers}>
+				<Text style={this.styles.answer}>{this.bytes * 2} bits</Text>
+				<Text style={this.styles.answer}>{this.bytes * 4} bits</Text>
+				<Text style={this.styles.answer}>{this.bytes * 8} bits</Text>
+				<Text style={this.styles.answer}>{this.bytes * 3} bits</Text>
+			  </View>
+			  <StatusBar style="auto" />
+			</View>
+		  );
+	}
+}
